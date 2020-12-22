eac8b1e63c21e783d66489fc7e3f94f7e69795f0
Julian Mastieri <julimastieri@gmail.com>
2020-12-22 09:18:48 -0300

    change web to extended

--- src/main/java/com/solvd/webTesting/gui/components/FooterMenu.java
+++ src/main/java/com/solvd/webTesting/gui/components/FooterMenu.java
@@ -2,21 +2,19 @@ package com.solvd.webTesting.gui.components;
 
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractUIObject;
 import com.solvd.webTesting.gui.pages.NewsPage;
 
 public class FooterMenu extends AbstractUIObject {
     
     @FindBy(linkText = "News")
-    private WebElement newsLink;
+    private ExtendedWebElement newsLink;
 
     public FooterMenu(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
-        PageFactory.initElements(driver, this);
     }
     
     public NewsPage openNewsPage() {
--- src/main/java/com/solvd/webTesting/gui/components/NavigationBar.java
+++ src/main/java/com/solvd/webTesting/gui/components/NavigationBar.java
@@ -2,28 +2,26 @@ package com.solvd.webTesting.gui.components;
 
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractUIObject;
 
 public class NavigationBar extends AbstractUIObject {
     
 	@FindBy(className="go")
-	private WebElement SearchButton;
+	private ExtendedWebElement SearchButton;
     
     @FindBy(id = "topsearch-text")
-    private WebElement SearchBox;
+    private ExtendedWebElement SearchBox;
 	
 	public NavigationBar(WebDriver driver, SearchContext searchContext) {
 		super(driver, searchContext);
-		PageFactory.initElements(driver, this);
 	}
 	
 	public void search(String product) {
 		SearchBox.click();
-		SearchBox.sendKeys(product);
+		SearchBox.type(product);
 		SearchButton.click();
 	}
 }
